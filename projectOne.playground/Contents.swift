779406c649bb8b8e29e89795c8ab31984037b0e0
Tyler Hart <tyhart90@yahoo.com>
2018-04-10 20:55:59 -0700

    added playground

---
+++ projectOne.playground/Contents.swift
@@ -0,0 +1,8 @@
+// Soccer League Project
+
+let player1: [String : Any] = ["name": "Joe Smith", "height": 42.0, "experience": true, "gaurdians": "Jim and Jan Smith"]
+let player2: [String : Any] = ["name": "Jill Tanner", "height": 36.0, "experience": true, "gaurdians": "Clara Tanner"]
+let player3: [String : Any] = ["name": "Bill Bon", "height": 43.0, "experiance": true, "gaurdians": "Sara and Jenny Bon"]
+print(player1)
+
+
